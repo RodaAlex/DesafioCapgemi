3f3f0cb7f0866e3e6b44ed66e2cdb549cfd7407b
RodaAlex <99984918+RodaAlex@users.noreply.github.com>
2022-02-18 13:37:43 -0300

    Add files via upload

---
+++ numb.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int i;
+  int j;
+  int k;
+  int n; 		//Numero de linhas 
+  
+  cout << "n= "; 
+  cin >> n; 	// Pega o valor n com o usuario
+  i = 1;
+  while (i <= n) {
+  	j = 0;	//Redefine os valores de j e k em cada ciclo
+  	k = n;
+  	
+	  while (k > i) {  	// Escreve os caracteres " "
+	  	cout << " " ;
+		k--;
+	  }
+  	
+	  while (j < i) { 	// Escreve os caracteres "*"
+	  	cout << "*" ;
+		j++;
+	  }
+ 
+  	cout << "\n";	// Pula a linha e volta o loop
+  	i++;
+  }
+  
+  return 0;
+}
+
